9ed08e551e54c279da6cc1680000803dd1f9eb7a
yjlhz <2386987985@qq.com>
2022-01-17 16:31:28 +0800

    整合swagger2

--- src/main/java/com/yjlhz/doademo/pojo/Course.java
+++ src/main/java/com/yjlhz/doademo/pojo/Course.java
@@ -17,8 +17,12 @@ public class Course implements Serializable {
     @ApiModelProperty("课程id")
     private Integer courseId;
 
+    @ApiModelProperty("课程名称")
     private String courseName;
 
+    @ApiModelProperty("课程学分")
     private Double courseCredit;
+
+    private static final long serialVersionUID = 1L;
 
 }
