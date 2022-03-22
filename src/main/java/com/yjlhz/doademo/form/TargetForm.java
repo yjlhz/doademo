b6a884f5a3e98b45e8bdc0b87a7f1952db1f94e4
yjlhz <2386987985@qq.com>
2022-03-22 23:56:02 +0800

    业务逻辑

---
+++ src/main/java/com/yjlhz/doademo/form/TargetForm.java
@@ -0,0 +1,26 @@
+package com.yjlhz.doademo.form;
+
+import lombok.Data;
+
+/**
+ * @author lhz
+ * @title: TargetForm
+ * @projectName doademo
+ * @description: 课程目标提交表单
+ * @date 2022/3/22 23:25
+ */
+
+@Data
+public class TargetForm {
+
+    private String targetName;
+
+    private Integer targetNo;
+
+    private Integer planId;
+
+    private String description;
+
+    private Integer secondRequirementId;
+
+}
---
+++ src/main/java/com/yjlhz/doademo/pojo/Target.java
@@ -0,0 +1,38 @@
+package com.yjlhz.doademo.pojo;
+
+import io.swagger.annotations.ApiModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * @author lhz
+ * @title: Target
+ * @projectName doademo
+ * @description: 课程目标实体类
+ * @date 2022/3/22 23:20
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ApiModel("课程目标实体类")
+public class Target implements Serializable {
+
+    private Integer targetId;
+
+    private String targetName;
+
+    private Integer targetNo;
+
+    private Integer planId;
+
+    private String description;
+
+    private Integer secondRequirementId;
+
+    private static final long serialVersionUID = 1L;
+
+}
