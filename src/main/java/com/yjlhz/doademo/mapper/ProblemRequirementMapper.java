0451822761351012f9baf278badb70fa150810a6
yjlhz <2386987985@qq.com>
2022-03-23 21:12:44 +0800

    增加实体类

---
+++ src/main/java/com/yjlhz/doademo/mapper/ProblemRequirementMapper.java
@@ -0,0 +1,18 @@
+package com.yjlhz.doademo.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author lhz
+ * @title: ProblemRequirementMapper
+ * @projectName doademo
+ * @description: 绑定问题和指标点的dao接口
+ * @date 2022/3/23 21:11
+ */
+
+@Mapper
+@Repository
+public interface ProblemRequirementMapper {
+
+}
