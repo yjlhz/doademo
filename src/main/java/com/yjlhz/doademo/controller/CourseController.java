b5c3b84a5d6f58927efb2936dcbbcea812055914
yjlhz <2386987985@qq.com>
2022-04-29 20:10:02 +0800

    整合前端

--- src/main/java/com/yjlhz/doademo/controller/CourseController.java
+++ src/main/java/com/yjlhz/doademo/controller/CourseController.java
@@ -1,5 +1,7 @@
 package com.yjlhz.doademo.controller;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.yjlhz.doademo.enums.ResultEnum;
 import com.yjlhz.doademo.form.CourseForm;
 import com.yjlhz.doademo.mapper.CourseMapper;
@@ -10,18 +12,22 @@ import com.yjlhz.doademo.vo.ResultVO;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.util.List;
 
-@RestController
+@Controller
 @Slf4j
 @RequestMapping("/course")
 public class CourseController {
@@ -42,8 +48,36 @@ public class CourseController {
     }
 
     @GetMapping("/queryCourseList")
-    public ResultVO queryCourseList(){
-        return courseService.queryCourses();
+    public String queryCourseList(Model model,
+                                  @RequestParam(required = false,defaultValue="1",value="pageNum")Integer pageNum,
+                                  @RequestParam(defaultValue="2",value="pageSize")Integer pageSize){
+        //为了程序的严谨性，判断非空：
+        //设置默认当前页
+        if(pageNum==null || pageNum<=0){
+            pageNum = 1;
+        }
+        //设置默认每页显示的数据数
+        if(pageSize == null){
+            pageSize = 1;
+        }
+        System.out.println("当前页是："+pageNum+"显示条数是："+pageSize);
+
+        //1.引入分页插件,pageNum是第几页，pageSize是每页显示多少条,默认查询总数count
+        PageHelper.startPage(pageNum,pageSize);
+        //2.紧跟的查询就是一个分页查询-必须紧跟.后面的其他查询不会被分页，除非再次调用PageHelper.startPage
+        try {
+            List<Course> courseList = (List<Course>) courseService.queryCourses().getData();
+            System.out.println("分页数据："+courseList);
+            //3.使用PageInfo包装查询后的结果,5是连续显示的条数,结果list类型是Page<E>
+            PageInfo<Course> pageInfo = new PageInfo<Course>(courseList,pageSize);
+            //4.使用model传参数回前端
+            model.addAttribute("pageInfo",pageInfo);
+            model.addAttribute("courseList",courseList);
+        }finally {
+            //清理 ThreadLocal 存储的分页参数,保证线程安全
+            PageHelper.clearPage();
+        }
+        return "list";
     }
 
     @PostMapping("/addCourse")
@@ -63,8 +97,10 @@ public class CourseController {
     }
 
     @GetMapping("/deleteCourse")
-    public ResultVO deleteCourse(Integer courseId){
-        return courseService.deleteCourse(courseId);
+    public String deleteCourse(Integer courseId){
+        ResultVO resultVO = courseService.deleteCourse(courseId);
+
+        return "";
     }
 
     @GetMapping("/queryCourserByName")
