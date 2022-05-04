e3e535d9bd11ae3b6d31a087625fb76b4a7761d9
yjlhz <2386987985@qq.com>
2022-05-04 17:06:56 +0800

    整合前端（考核成绩部分）

--- src/main/java/com/yjlhz/doademo/controller/MatrixController.java
+++ src/main/java/com/yjlhz/doademo/controller/MatrixController.java
@@ -1,24 +1,17 @@
 package com.yjlhz.doademo.controller;
 
-import com.yjlhz.doademo.enums.ResultEnum;
 import com.yjlhz.doademo.form.MatrixForm;
-import com.yjlhz.doademo.form.ProblemListForm;
-import com.yjlhz.doademo.form.ProblemObjectiveForm;
 import com.yjlhz.doademo.form.WeightForm;
 import com.yjlhz.doademo.pojo.Examine;
 import com.yjlhz.doademo.service.ExamineService;
 import com.yjlhz.doademo.service.MatrixService;
-import com.yjlhz.doademo.utils.ResultVOUtil;
-import com.yjlhz.doademo.vo.ResultVO;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -58,9 +51,14 @@ public class MatrixController {
     }
 
     @PostMapping("/addWeight")
-    String addWeight(@Param("examineId")Integer[] examineId, @Param("weight")Double[] weight){
+    String addWeight(@Param("examineId")Integer[] examineId,@Param("weight")Double[] weight){
         List<WeightForm> weightForms = new ArrayList<>();
-
+        for (int i = 0;i<examineId.length;i++){
+            WeightForm weightForm = new WeightForm();
+            weightForm.setExamineId(examineId[i]);
+            weightForm.setWeight(weight[i]);
+            weightForms.add(weightForm);
+        }
         matrixService.addWeight(weightForms);
         return "redirect:/examine/queryExamineList";
     }
