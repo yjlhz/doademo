8de6fbcb8262993920323937efb07c6ce3b358ba
yjlhz <2386987985@qq.com>
2022-05-08 17:52:33 +0800

    修复bug

--- src/main/java/com/yjlhz/doademo/service/impl/CalculateServiceImpl.java
+++ src/main/java/com/yjlhz/doademo/service/impl/CalculateServiceImpl.java
@@ -65,104 +65,169 @@ public class CalculateServiceImpl implements CalculateService {
         int res = -1;
         //获取课程培养目标
         List<Objective> objectives = objectiveMapper.queryByPlanIdAndCourseId(planId, courseId);
-        //计算每个目标达成度
+        //遍历课程目标
         for (Objective objective : objectives){
-            //根据目标id获取支撑目标的问题id
+            //查找支撑该指标点的所有问题
             List<ProblemObjective> problemObjectives = problemObjectiveMapper.queryByObjectiveId(objective.getObjectiveId());
-            Map<Integer,List<Double>> data = new HashMap<>();
-            //计算每个问题达成度
-            double sumMax = 0;//支撑该目标的问题总分
+            //根据examineId进行分类
+            Map<Integer,List<Problem>> classifyMap = new HashMap<>();
             for (ProblemObjective problemObjective : problemObjectives){
                 Problem problem = problemMapper.queryById(problemObjective.getProblemId());
-                sumMax = ArithUtil.add(sumMax,problem.getMaxScore());
-            }
-            for (ProblemObjective problemObjective : problemObjectives){
-                List<StudentProblem> studentProblems = studentProblemMapper.queryStudentProblemsByProblemId(problemObjective.getProblemId());
-                double sum = 0;
-                int count = 0;
+                //计算每个问题达成度
+                List<StudentProblem> studentProblems = studentProblemMapper.queryStudentProblemsByProblemId(problem.getId());
+                Double getSumScore = 0.0;
+                int countStudents = 0;
                 for (StudentProblem studentProblem : studentProblems){
-                    sum = ArithUtil.add(sum,studentProblem.getScore());
-                    count++;
+                    getSumScore = ArithUtil.add(getSumScore,studentProblem.getScore());
+                    countStudents++;
                 }
-                Problem problem = problemMapper.queryById(problemObjective.getProblemId());
-                double achieve = ArithUtil.div(sum,ArithUtil.mul(count,problem.getMaxScore()));
+                Double achieve = ArithUtil.div(getSumScore,ArithUtil.mul(problem.getMaxScore(),countStudents));
                 problem.setAchieve(achieve);
-                problemMapper.updateProblem(problem);
-                achieve = ArithUtil.mul(achieve,ArithUtil.div(problem.getMaxScore(),sumMax));;
-                data.put(problem.getExamineId(),data.getOrDefault(problem.getExamineId(),new ArrayList<>()));
-                List<Double> doubles = data.get(problem.getExamineId());
-                doubles.add(achieve);
-                data.put(problem.getExamineId(),doubles);
+                res = problemMapper.updateProblem(problem);
+                Integer examineId = problem.getExamineId();
+                List<Problem> problemList = classifyMap.getOrDefault(examineId, new ArrayList<>());
+                problemList.add(problem);
+                classifyMap.put(examineId,problemList);
             }
-            double flag = 1;
-            double endAchieve = 0;
-            double result = 0;
-            //判断用户是否全部缺省权重，如果全部缺省，则按分数自动算
-            int c = 0;
-            int keys = 0;
-            for (Map.Entry entry : data.entrySet()){
-                Integer key = (Integer) entry.getKey();
-                if (examineMapper.queryExamineById(key).getWeight() == 0.0){
-                    c++;
-                }
-                keys++;
+            //计算总的examine权重
+            Double sumWeight = 0.0;
+            for (Map.Entry entry : classifyMap.entrySet()){
+                //examineId
+                Integer examineId = (Integer) entry.getKey();
+                sumWeight = ArithUtil.add(sumWeight,examineMapper.queryExamineById(examineId).getWeight());
             }
-            //如果权重不是全部缺省的时候，要减去权重为0的总分
-            if (c!=keys){
-                for (Map.Entry entry : data.entrySet()){
-                    Integer key = (Integer) entry.getKey();
-                    Examine examine = examineMapper.queryExamineById(key);
-                    if (examine.getWeight() == 0.0){
-                        for (ProblemObjective problemObjective : problemObjectives){
-                            Problem problem = problemMapper.queryById(problemObjective.getProblemId());
-                            if (problem.getExamineId() == examine.getId()){
-                                sumMax = ArithUtil.sub(sumMax,problem.getMaxScore());
-                            }
-                        }
-                    }
+            Map<Integer,Double> weightMap = new HashMap<>();
+            for (Map.Entry entry : classifyMap.entrySet()){
+                //examineId
+                Integer examineId = (Integer) entry.getKey();
+                //重新划定权重值
+                Double weight = ArithUtil.div(examineMapper.queryExamineById(examineId).getWeight(),sumWeight);
+                weightMap.put(examineId,weight);
+            }
+            //计算problem权重
+            Double sumScore = 0.0;
+            //算所有问题总分
+            Map<Integer,Double> problemSumMap = new HashMap<>();
+            for (Map.Entry entry : classifyMap.entrySet()){
+                List<Problem> problems = (List<Problem>) entry.getValue();
+                Integer eId = 0;
+                for (Problem p : problems){
+                    sumScore = ArithUtil.add(sumScore,p.getMaxScore());
+                    eId = p.getExamineId();
                 }
-                data.clear();//如果出现sumMax变化了，重新计算
-                for (ProblemObjective problemObjective : problemObjectives){
-                    List<StudentProblem> studentProblems = studentProblemMapper.queryStudentProblemsByProblemId(problemObjective.getProblemId());
-                    double sum = 0;
-                    int count = 0;
-                    for (StudentProblem studentProblem : studentProblems){
-                        sum = ArithUtil.add(sum,studentProblem.getScore());
-                        count++;
-                    }
-                    Problem problem = problemMapper.queryById(problemObjective.getProblemId());
-                    double achieve = ArithUtil.div(sum,ArithUtil.mul(count,problem.getMaxScore()));
-                    achieve = ArithUtil.mul(achieve,ArithUtil.div(problem.getMaxScore(),sumMax));;
-                    data.put(problem.getExamineId(),data.getOrDefault(problem.getExamineId(),new ArrayList<>()));
-                    List<Double> doubles = data.get(problem.getExamineId());
-                    doubles.add(achieve);
-                    data.put(problem.getExamineId(),doubles);
+                problemSumMap.put(eId,sumScore);
+                sumScore = 0.0;
+            }
+            Double endAchieve = 0.0;
+            for (Map.Entry entry : classifyMap.entrySet()){
+                List<Problem> problems = (List<Problem>) entry.getValue();
+                for (Problem p : problems){
+                    Double pWeight = ArithUtil.mul(weightMap.get(problemMapper.queryById(p.getId()).getExamineId()),ArithUtil.div(p.getMaxScore(),problemSumMap.get(p.getExamineId())));
+                    endAchieve = ArithUtil.add(endAchieve,ArithUtil.mul(pWeight,p.getAchieve()));
                 }
             }
-            for (Map.Entry entry : data.entrySet()){
-                Integer key = (Integer) entry.getKey();
-                List<Double> values = (List<Double>) entry.getValue();
-                Examine examine = examineMapper.queryExamineById(key);
-                if (examine.getWeight() != 0.0){
-                    flag = examine.getWeight();
-                }else if (examine.getWeight() == 0.0 && c==keys){
-                    flag = 1;
-                }else if (examine.getWeight() == 0.0){
-                    flag = 0.0;
-                }
-                for (Double d : values){
-                    endAchieve = ArithUtil.add(endAchieve,d);
-                }
-                endAchieve = ArithUtil.mul(endAchieve,flag);
-                result =  ArithUtil.add(endAchieve,result);
-            }
-            objective.setAchieve(result);
+            objective.setAchieve(endAchieve);
             res = objectiveMapper.updateObjective(objective);
         }
-        if (res == -1){
-            return ResultVOUtil.error(ResultEnum.SERVER_ERROR);
-        }
-        return ResultVOUtil.success(objectives);
+        return ResultVOUtil.success();
+//        //计算每个目标达成度
+//        for (Objective objective : objectives){
+//            //根据目标id获取支撑目标的问题id
+//            List<ProblemObjective> problemObjectives = problemObjectiveMapper.queryByObjectiveId(objective.getObjectiveId());
+//            Map<Integer,List<Double>> data = new HashMap<>();
+//            //计算每个问题达成度
+//            double sumMax = 0;//支撑该目标的问题总分
+//            for (ProblemObjective problemObjective : problemObjectives){
+//                Problem problem = problemMapper.queryById(problemObjective.getProblemId());
+//                sumMax = ArithUtil.add(sumMax,problem.getMaxScore());
+//            }
+//            for (ProblemObjective problemObjective : problemObjectives){
+//                List<StudentProblem> studentProblems = studentProblemMapper.queryStudentProblemsByProblemId(problemObjective.getProblemId());
+//                double sum = 0;
+//                int count = 0;
+//                for (StudentProblem studentProblem : studentProblems){
+//                    sum = ArithUtil.add(sum,studentProblem.getScore());
+//                    count++;
+//                }
+//                Problem problem = problemMapper.queryById(problemObjective.getProblemId());
+//                double achieve = ArithUtil.div(sum,ArithUtil.mul(count,problem.getMaxScore()));
+//                problem.setAchieve(achieve);
+//                problemMapper.updateProblem(problem);
+//                achieve = ArithUtil.mul(achieve,ArithUtil.div(problem.getMaxScore(),sumMax));;
+//                data.put(problem.getExamineId(),data.getOrDefault(problem.getExamineId(),new ArrayList<>()));
+//                List<Double> doubles = data.get(problem.getExamineId());
+//                doubles.add(achieve);
+//                data.put(problem.getExamineId(),doubles);
+//            }
+//            double flag = 1;
+//            double endAchieve = 0;
+//            double result = 0;
+//            //判断用户是否全部缺省权重，如果全部缺省，则按分数自动算
+//            int c = 0;
+//            int keys = 0;
+//            for (Map.Entry entry : data.entrySet()){
+//                Integer key = (Integer) entry.getKey();
+//                if (examineMapper.queryExamineById(key).getWeight() == 0.0){
+//                    c++;
+//                }
+//                keys++;
+//            }
+//            //如果权重不是全部缺省的时候，要减去权重为0的总分
+//            if (c!=keys){
+//                for (Map.Entry entry : data.entrySet()){
+//                    Integer key = (Integer) entry.getKey();
+//                    Examine examine = examineMapper.queryExamineById(key);
+//                    if (examine.getWeight() == 0.0){
+//                        for (ProblemObjective problemObjective : problemObjectives){
+//                            Problem problem = problemMapper.queryById(problemObjective.getProblemId());
+//                            if (problem.getExamineId() == examine.getId()){
+//                                sumMax = ArithUtil.sub(sumMax,problem.getMaxScore());
+//                            }
+//                        }
+//                    }
+//                }
+//                data.clear();//如果出现sumMax变化了，重新计算
+//                for (ProblemObjective problemObjective : problemObjectives){
+//                    List<StudentProblem> studentProblems = studentProblemMapper.queryStudentProblemsByProblemId(problemObjective.getProblemId());
+//                    double sum = 0;
+//                    int count = 0;
+//                    for (StudentProblem studentProblem : studentProblems){
+//                        sum = ArithUtil.add(sum,studentProblem.getScore());
+//                        count++;
+//                    }
+//                    Problem problem = problemMapper.queryById(problemObjective.getProblemId());
+//                    double achieve = ArithUtil.div(sum,ArithUtil.mul(count,problem.getMaxScore()));
+//                    achieve = ArithUtil.mul(achieve,ArithUtil.div(problem.getMaxScore(),sumMax));;
+//                    data.put(problem.getExamineId(),data.getOrDefault(problem.getExamineId(),new ArrayList<>()));
+//                    List<Double> doubles = data.get(problem.getExamineId());
+//                    doubles.add(achieve);
+//                    data.put(problem.getExamineId(),doubles);
+//                }
+//            }
+//            for (Map.Entry entry : data.entrySet()){
+//                Integer key = (Integer) entry.getKey();
+//                List<Double> values = (List<Double>) entry.getValue();
+//                Examine examine = examineMapper.queryExamineById(key);
+//                if (examine.getWeight() != 0.0){
+//                    flag = examine.getWeight();
+//                }else if (examine.getWeight() == 0.0 && c==keys){
+//                    flag = 1;
+//                }else if (examine.getWeight() == 0.0){
+//                    flag = 0.0;
+//                }
+//                for (Double d : values){
+//                    endAchieve = ArithUtil.add(endAchieve,d);
+//                }
+//                endAchieve = ArithUtil.mul(endAchieve,flag);
+//                result =  ArithUtil.add(endAchieve,result);
+//            }
+//            objective.setAchieve(result);
+//            res = objectiveMapper.updateObjective(objective);
+//        }
+//        if (res == -1){
+//            return ResultVOUtil.error(ResultEnum.SERVER_ERROR);
+//        }
+//        return ResultVOUtil.success(objectives);
     }
 
     @Override
